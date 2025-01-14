21b6f054fb0cbb8a92f730daf683c29e428a3f2c
Vpretto0 <victoriapretto035@gmail.com>
2025-01-14 01:53:34 -0600

    no funciona python

--- law_and_order.py
+++ law_and_order.py
@@ -13,14 +13,17 @@ import pymysql
 class law_and_order:
     
     def __init__(self, root):
+        
+        root.overrideredirect(True) #quitar la barra de arriba
+        
         #editar la parte de arriba, para que solo se vea el titulo y ajustar las posiciones y loas tamaños correctos
         self.root = root
         titlespace = " "
         self.root.title(102 * titlespace + "THE LAW AND ORDER")
-        self.root.geometry("800x700+50+50") # width x height + X coordinate + Y coordinate
+        self.root.geometry("800x613+50+50") # width x height + X coordinate + Y coordinate
         self.root.resizable(width =False, height =False)
         
-        MainFrame = Frame(self.root, bd =10, width =770, height =700, relief = RIDGE, bg = '#1f1f1f')
+        MainFrame = Frame(self.root, bd =10, width =770, height =700, relief = RIDGE, bg = '#1f1f1f', fg= '#1f1f1f')
         MainFrame.grid()
         
         TitleFrame = Frame(MainFrame, bd =7, width =770, height =100, relief = RIDGE)
@@ -28,12 +31,12 @@ class law_and_order:
         TopFrame3 = Frame(MainFrame, bd =5, width =770, height =500, relief = RIDGE)
         TopFrame3.grid(row =1, column =0)
         
-        LeftFrame = Frame(TopFrame3, bd =5, width =770, height =400,padx =2, relief = RIDGE, bg = '#1f1f1f')
+        LeftFrame = Frame(TopFrame3, bd =5, width =770, height =400,padx =2, relief = RIDGE, bg = '#1f1f1f', fg= '#1f1f1f')
         LeftFrame.pack(side =LEFT)
         LeftFrame1 = Frame(LeftFrame, bd =5, width =600, height =180,padx =2, pady =4, relief = RIDGE)
         LeftFrame1.pack(side =TOP, padx =0, pady =0)
         
-        RightFrame1 = Frame(TopFrame3, bd =5, width =100, height =400,padx =2, relief = RIDGE, bg = '#1f1f1f')
+        RightFrame1 = Frame(TopFrame3, bd =5, width =100, height =400,padx =2, relief = RIDGE, bg = '#1f1f1f', fg= '#1f1f1f')
         RightFrame1.pack(side =RIGHT)
         RightFrame1a = Frame(RightFrame1, bd =5, width =90, height =300,padx =2, pady =2, relief = RIDGE)
         RightFrame1a.pack(side =TOP, padx =0, pady =0)
@@ -232,25 +235,25 @@ class law_and_order:
         self.people_records.bind("<ButtonRelease-1>", traineeinfo)
         DisplayData()
         #___________________________________________________BUTTONS____________________________________________________#
-        self.btnAddNew = Button(RightFrame1a, text = "Add Data", font =('courier', 14, 'bold'),
+        self.btnAddNew = Button(RightFrame1a, text = "Add Data", font =('courier', 14, 'bold'), fg='white', bg = '#212121', activebackground='gray',
             padx =15, pady=2, width =8, height =2,command =addData, bd =4). grid(row =0, column =0, padx =0)
         
-        self.btnAddNew = Button(RightFrame1a, text = "Display", font =('courier', 14, 'bold'),
+        self.btnAddNew = Button(RightFrame1a, text = "Display", font =('courier', 14, 'bold'), fg='white', bg = '#212121', activebackground='gray',
             padx =15, pady=2, width =8, height =2,command =DisplayData, bd =4). grid(row =1, column =0, padx =0)
         
-        self.btnAddNew = Button(RightFrame1a, text = "Update", font =('courier', 14, 'bold'),
+        self.btnAddNew = Button(RightFrame1a, text = "Update", font =('courier', 14, 'bold'), fg='white', bg = '#212121', activebackground='gray',
             padx =15, pady=2, width =8, height =2,command =update, bd =4). grid(row =2, column =0, padx =0)
         
-        self.btnAddNew = Button(RightFrame1a, text = "Delete", font =('courier', 14, 'bold'),
+        self.btnAddNew = Button(RightFrame1a, text = "Delete", font =('courier', 14, 'bold'), fg='white', bg = '#212121', activebackground='gray',
             padx =15, pady=2, width =8, height =2, command= delete, bd =4). grid(row =3, column =0, padx =0)
         
-        self.btnAddNew = Button(RightFrame1a, text = "Search", font =('courier', 14, 'bold'),
+        self.btnAddNew = Button(RightFrame1a, text = "Search", font =('courier', 14, 'bold'), fg='white', bg = '#212121', activebackground='gray',
             padx =15, pady=2, width =8, height =2, command =searchdb, bd =4). grid(row =4, column =0, padx =0)
         
-        self.btnAddNew = Button(RightFrame1a, text = "Reset", font =('courier', 14, 'bold'),
+        self.btnAddNew = Button(RightFrame1a, text = "Reset", font =('courier', 14, 'bold'), fg='white', bg = '#212121', activebackground='gray',
             padx =15, pady=2, width =8, height =2, command =Reset,  bd =4). grid(row =5, column =0, padx =0)
         
-        self.btnAddNew = Button(RightFrame1a, text = "EXIT", font =('courier', 14, 'bold'),
+        self.btnAddNew = Button(RightFrame1a, text = "EXIT", font =('courier', 14, 'bold'), fg = "red", bg = '#212121', activebackground='gray',
             padx =15, pady=2, width =8, height =2, command =iExit, bd =4). grid(row =6, column =0, padx =0)
         
         
