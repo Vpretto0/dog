bd3ce8d0073e34f2e23aadcb604f629ee72b1d95
Vpretto0 <victoriapretto035@gmail.com>
2025-01-20 23:52:07 -0600

    código nivel dios

--- photo.py
+++ photo.py
@@ -1,6 +1,7 @@
 
 from tkinter import ttk
 from tkinter import *
+from tkinter import filedialog
 import tkinter.messagebox
 
 import pymysql
@@ -8,8 +9,7 @@ import db_identity
 from db_identity import *
 
 import io
-from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageTk
 
 
     
@@ -21,12 +21,11 @@ class photo_class:
         style = ttk.Style()
         style.theme_use('clam')
         
-        
         root.configure(bg='#f1f1f1')
         self.root = root
         titlespace = " "
         self.root.title(102 * titlespace + "Photo Frame")
-        self.root.geometry("235x380+1080+380") # width x height + X coordinate + Y coordinate
+        self.root.geometry("241x395+1080+380") # width x height + X coordinate + Y coordinate
         self.root.resizable(width =False, height =False)
         self.root.configure(bg = '#1f1f1f')
         
@@ -47,12 +46,25 @@ class photo_class:
         #_______________________________________________________________________________________________________#
         
         self.lbltitle = Label(TitleFrame, font =('courier', 16, 'bold'),text ="PHOTO", bd =5, fg='white', bg='#1f1f1f')
-        self.lbltitle.grid(row =0, column =0, padx =62)
+        self.lbltitle.grid(row =0, column =0, padx =64)
         #_______________________________________________________________________________________________________#
         
             #182x228y
-        photo = Frame(TopFrame, bd =7, width =182, height =228,relief= RIDGE, bg = '#1f1f1f')
+        photo = Frame(TopFrame, bd =7, width =182, height =228,relief= RIDGE, bg = '#1f1f1f') 
         photo.pack()
+        #photo frame
+        
+        canvas = tkinter.Canvas(photo, background = 'black', width=182, height=228) # problema si introduces directamente esto: image= frame_image
+        canvas.grid(sticky = 'nsew')
+        #_______________________________________________________________________________________________________#
+        
+        
+        def load_image(data):
+            global frame_image
+            
+            photo_imagen=Image.open(data) 
+            photo_imagen= photo_imagen.resize((182, 228))
+            frame_image= ImageTk.PhotoImage(photo_imagen)
         
         
         def get_image():
@@ -63,7 +75,7 @@ class photo_class:
                     
                 try: 
                                
-                    fotito="SELECT photo FROM people WHERE id = '%s' "
+                    fotito="SELECT photo FROM people WHERE id = '86765' "
                     
                     #conn.commit()
                     c.execute(fotito)
@@ -74,8 +86,12 @@ class photo_class:
                         data= io.BytesIO(data_display[0][0])
                             #get first image from db and convert it to bytes   
                         imagen=Image.open(data)                   
-                        imagen.show()
-                        print("show_image function working")
+                        print("show_image function is working")
+                        
+                        load_image(data)
+                        canvas.create_image(0, 0, anchor="nw", image=frame_image)
+                        canvas.image = frame_image 
+                        
                         
                     else:
                         tkinter.messagebox.showinfo("Error", "no data ")
@@ -91,19 +107,59 @@ class photo_class:
 
             except Exception as e:
                 tkinter.messagebox.showinfo("Error", f"It's not working: {e}")
-                
-
-            def display_image():
-                pass
             
-        
+            
+        def add_image():
+            global file_path
+            file_path = filedialog.askopenfilename(initialdir="C:\images")
+                
+            new_image = Image.open(file_path)
+            width, height = int(182), int(228)
+            new_image = new_image.resize((width, height))
+            canvas.config(width=182, height=228)
+                
+            new_image = ImageTk.PhotoImage(new_image)
+                
+            canvas.new_image = new_image
+            canvas.create_image(0, 0, image=new_image, anchor="nw") 
+            
+            change_image(file_path, id=86765)
+
+        def change_image(file_path, id):
+            try:
+                print("Starting SAVE function")
+                conn, c  = db_identity.create_connection()
+                    
+                try:
+                    with open(file_path, 'rb') as file: # <"read binary">
+                        photo = file.read()
+                            
+                    sql = "UPDATE people SET photo = %s WHERE id = %s "
+                    c.execute(sql, (photo, id))
+                    conn.commit()
+                    print("IT'S WORKING!!")
+                        
+                except Exception as e:
+                    print(F"Error -> {e}")
+                    
+                finally:
+                    db_identity.close_connection(conn, c)
+                        
+            except Exception as e:
+                tkinter.messagebox.showinfo("Error", f"It's not working: {e}")
+        get_image()
+            
         #_______________________________________________________________________________________________________#
         
-        self.btnChange = Button(BottomFrame, text = "Change", font =('courier', 10, 'bold'), fg='white', bg = '#212121', activebackground='gray', command = show_image,
+        self.btnChange = Button(BottomFrame, text = "Change", font =('courier', 10, 'bold'), fg='white', bg = '#212121', activebackground='gray', command=add_image,
             padx =5, pady=1, width =8, height =1,  bd =5). grid(row =0, column =0, padx =3)
         
         self.btnDelete = Button(BottomFrame, text = "DELETE", font =('courier', 10, 'bold'), fg = "red", bg = '#212121', activebackground='red',
             padx =5, pady=1, width =8, height =1, bd =5). grid(row =0, column =1, padx =3)
+        #_______________________________________________________________________________________________________#
+        
+        
+        #canva
         #_______________________________________________________________________________________________________#
         
         
