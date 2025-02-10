4fbff31bfb0c31104770e98b4f204e35ec619bc3
Vpretto0 <victoriapretto035@gmail.com>
2025-02-09 18:06:20 -0600

    commmit porque funciona

--- raspi_exe.py
+++ raspi_exe.py
@@ -7,6 +7,7 @@ import db_identity_verification
 import db_identity
 
 import serial
+import socket
 import datetime
 
 #_____________________________________________________________________________________#
@@ -23,6 +24,7 @@ pase = False
 clase = "invalid"
 #_____________________________________________________________________________________#
 def while_running(verification_id, people_id):
+    print("\n\n\n\n\n\n\n\n")
     conn, c = db_identity.create_connection()
     
     try:    
@@ -33,8 +35,13 @@ def while_running(verification_id, people_id):
             c.execute("SELECT id FROM people WHERE id = %s", (verification_id,))
             print("verification id:", verification_id,". READED")
             fetch = c.fetchone()
-            people_id = int(fetch[0])
-            print("people id: ", people_id)
+            (people_id) = fetch
+            if people_id is not None:
+                people_id = int(fetch[0])
+                print("people id: ", people_id)
+            else:
+                print("people id: ", people_id)
+                
             
             if people_id == None:
                 print("This is your last chance, try again")
@@ -42,26 +49,26 @@ def while_running(verification_id, people_id):
                 c.execute("SELECT id FROM people WHERE id = %s", (verification_id2,))
                 people_id = c.fetchone()
                 print("people id: ", people_id)
-                
+
                 if people_id == None:
                     print("\n\nINTRUDE FOUND\n ...Initializing WARNING MODE\n")
                     arduino_communication_warning() #WARNING MODE
-                    get_dbinfo(0, "0", 0, verification_id)
                     print("people id: ", people_id)
+                    get_dbinfo(verification_id)
                     while_running(verification_id, people_id)
                     
                 else:
                     print("Match found, the communication is working")
                     arduino_communication_pass()
-                    get_dbinfo(0, "0", 0, verification_id)
+                    get_dbinfo(verification_id)
                     while_running(verification_id, people_id)
             else:
                 print("Match found, the communication is working")
                 arduino_communication_pass()
-                get_dbinfo(0, "0", 0, verification_id)
+                get_dbinfo(verification_id)
                 while_running(verification_id, people_id)
     except Exception as e:
-        print(f"Error from correct_id: {e}")
+        print(f"Error from while_running: {e}")
     
     finally:
         db_identity.close_connection(conn, c)
@@ -69,40 +76,51 @@ def while_running(verification_id, people_id):
         verification_id = scanner_input      #id from scanner
         people_id = 0
         pase = False
-def get_dbinfo(tiempo, ip, info, id):
+        
+        
+        
+def get_dbinfo(id):
     try:
         connn, cc = db_identity_verification.create_connection()
         conn, c = db_identity.create_connection()
         
-        #________________________________________________TIEMPO________________________________________________#
+        #time
         now = datetime.datetime.now()
         tiempo = now
         print(f"Time: {tiempo}")
-        #________________________________________________IPv6  ________________________________________________#
         
-        #________________________________________________PASE  ________________________________________________#
-        #boolean
+        
+        #ip
+        get_ip = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+        get_ip.connect(("2001:4860:4860::8888", 69)) #google dns, ramdom port
+        ip = get_ip.getsockname()[0] #ipv6
+        print(f"IPv6: {ip}")
+        
+
+        #boolean/pase
         global pase
-        #________________________________________________INFO  ________________________________________________#
         
-        
+        #info
         get_info = cc.execute("SELECT count(*) FROM db_identity.verification WHERE true = true")
         get_info = cc.fetchone()
         info_base= int(get_info[0])
         info = info_base + 1
         print(info)
         
-        #________________________________________________ID FK ________________________________________________#
+        
+        #id
         try:
             get_id = c.execute("SELECT id FROM people WHERE id = %s", (id,))
             get_id = c.fetchone()
-            if id is not None:
+            if get_id is not None:
                 id = int(get_id[0])
             else:
                 id = None
         except Exception as e:
-            print(f"No id:{id}Found,{e}")
-        #________________________________________________CLASE ________________________________________________#
+            print(f"No id:{id} Found,{e}")
+            
+            
+        #CLASES   
         #clases: student, staff, guest, invalid.
         global clase
         
@@ -111,21 +129,17 @@ def get_dbinfo(tiempo, ip, info, id):
             get_class = c.fetchone()
             clase = str(get_class[0])
         else:
-            clase = "invalid"
-        #________________________________________________INSERT INTO___________________________________________#
+            clase = "invalid" 
+            
+        #inserting  db
         cc.execute("INSERT INTO verification (date_time, ip_ipv6, pass, class, info, id) VALUES (%s, %s, %s, %s, %s, %s)", (tiempo, ip, pase, clase, info, id,))
+        print(f"Uploading info from database FINALLY STEP: {tiempo}, {ip}, {pase}, {clase}, {info}, {id}")
         connn.commit()
     except Exception as e:
         print("Error from get_dbinfo in general{e}")
     finally: 
-        try:
-            print(f"Uploading info from database FINALLY STEP: {tiempo}, {ip}, {pase}, {clase}, {info}, {id}") 
-            connn, cc = db_identity_verification.create_connection()
-            conn, c = db_identity.create_connection()  
-        except Exception as e:
-            print(f"Error from get_dbinfo_finally step{e}")
-            connn, cc = db_identity_verification.create_connection()
-            conn, c = db_identity.create_connection() 
+        connn, cc = db_identity_verification.create_connection()
+        conn, c = db_identity.create_connection()   
 
 def arduino_communication_tryagain():
     print("TRY AGAIN MODE")
